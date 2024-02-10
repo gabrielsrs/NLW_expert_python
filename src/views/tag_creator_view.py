78d4533312d1af240713a647feb516595752fa59
Gabriel Reis <gabsrs@gitbasic.com>
2024-02-10 11:22:35 -0300

    feat: add controller and driver Add driver to manipulate the barcode lib Create controller to lead with business rules

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,14 +1,17 @@
 from .http_types.http_requests import HttpRequest
 from ..views.http_types.http_responses import HttpResponse
+from ..controllers.tag_creator_controller import  TagCreateController
 
 class TagCreatorView:
     """
         Responsibility for interact with HTTP
     """
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        tag_creator_controller = TagCreateController()
+
         body = http_request.body
         product_code = body["product_code"]
 
-        print(product_code)
+        formatted_response = tag_creator_controller.create(product_code)
 
-        return HttpResponse(status_code=200, body={})
+        return HttpResponse(status_code=200, body=formatted_response)
